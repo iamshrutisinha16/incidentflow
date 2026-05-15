85cf55a62c0402a297efff2009f6cef15265e130
shrutisinha74168 <shrutisinha74168@gmail.com>
2026-05-16 01:21:41 +0530

    Fixed get incidents API

--- backend/controllers/incidentController.js
+++ backend/controllers/incidentController.js
@@ -1,18 +1,25 @@
-const getIncidents = (req, res) => {
-  res.json({
-    success: true,
-    message: "Get incidents working",
-  });
+const Incident = require("../models/Incident");
+
+const getIncidents = async (req, res) => {
+
+  try {
+
+    const incidents = await Incident.find({
+      tenantId: req.user.tenantId,
+    }).sort({ createdAt: -1 });
+
+    res.status(200).json(incidents);
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch incidents",
+    });
+
+  }
 };
 
-const createIncident = (req, res) => {
-  res.json({
-    success: true,
-    message: "Create incident working",
-  });
-};
+module.exports = { getIncidents,
 
-module.exports = {
-  getIncidents,
-  createIncident,
 };
